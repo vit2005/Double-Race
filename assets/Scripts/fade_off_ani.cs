a5f0189abd3dcfd6ea7c4c081fccd177175998f3
Vit <elstan88@mail.ru>
2016-09-28 00:32:34 +0300

    1.32

--- assets/Scripts/fade_off_ani.cs
+++ assets/Scripts/fade_off_ani.cs
@@ -4,11 +4,15 @@ using System.Collections;
 
 public class fade_off_ani : MonoBehaviour {
 
+	static bool already = false;
 	public Color c;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (already)
+			Destroy (this);
+		else
+			already = true;
 	}
 	
 	// Update is called once per frame
